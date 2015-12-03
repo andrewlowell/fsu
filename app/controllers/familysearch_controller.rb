9ab70f2f132ae912bfd4e46b1ed5172b930f8535
Andrew <mynock101@gmail.com>
2015-12-02 18:50:58 -0700

    Change authentication flow to automatically get a token if the old one has expired.

--- app/controllers/familysearch_controller.rb
+++ app/controllers/familysearch_controller.rb
@@ -4,22 +4,38 @@ class FamilysearchController < ApplicationController
 
   def auth
     @user = current_user
-    @hash = auth_hash
-    @user.update_attribute(:keyfs, @hash[:credentials][:token])
-    redirect_to root_path
+    @auth = auth_hash
+    @user.update_attribute(:keyfs, @auth[:credentials][:token])
+
+    @params = request.env["omniauth.params"]
+
+    if @params["controller"] == "postpic"
+      redirect_to "/familysearch/post_pic/#{@params["id"]}/#{@params["filename"]}"
+    elsif @params["controller"] == "createdescription"
+      redirect_to "/familysearch/create_description/#{@params["id"]}/#{@params["title"]}/#{@params["note"]}"
+    end
+
   end
 
   def post_pic
     @user = current_user
     @image = Image.find(params[:id])
     @rando = ('a'..'z').to_a.shuffle[0,4].join
-    HTTP.auth("Bearer #{@user.keyfs}")
+
+    @code = HTTP.auth("Bearer #{@user.keyfs}")
       .headers('Content-Type' => 'image/jpeg', 'Content-Disposition' => "#{params[:filename]}")
       .post("https://sandbox.familysearch.org/platform/memories/memories",
         :form => { :type => 'Photo' },
         :params => { :title => "#{@rando}" },
-        :body => "#{HTTP::FormData::File.new("#{@image.data.path(:original)}")}" )
-    redirect_to root_path
+        :body => "#{HTTP::FormData::File.new("#{@image.data.path(:original)}")}"
+      ).code
+
+      if @code == 401
+        redirect_to "/auth/familysearch?controller=postpic&id=#{params[:id]}&filename=#{params[:filename]}"
+      else
+        redirect_to root_path
+      end
+
   end
 
   def create_description
@@ -28,13 +44,17 @@ class FamilysearchController < ApplicationController
     @note = params[:note]
     @url = "http://0.0.0.0:3000/images/#{params[:id]}"
 
-    puts HTTP.auth("Bearer #{@user.keyfs}")
+    @code =  HTTP.auth("Bearer #{@user.keyfs}")
       .headers('Content-Type' => 'application/x-gedcomx-v1+json')
       .post("https://sandbox.familysearch.org/platform/sources/descriptions",
         :body => create_description_json("Here's a new citation, friends!", @url, @title, @note)
-        )
+      ).code
 
-    redirect_to root_path
+    if @code == 401
+      redirect_to "/auth/familysearch?controller=createdescription&id=#{params[:id]}&title=#{URI.encode_www_form_component(params[:title])}&note=#{URI.encode_www_form_component(params[:note])}"
+    else
+      redirect_to root_path
+    end
 
   end
 
